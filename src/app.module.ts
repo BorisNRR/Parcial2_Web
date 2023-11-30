797ca7a996ee66fa66b986b9f379a0a591e1f3d8
Boris Reyes <b.reyesr@uniandes.edu.co>
2023-11-30 09:37:07 -0500

    Se crean modulos y entidades del modelo de clases

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { RedSocialModule } from './red-social/red-social.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { FotoModule } from './foto/foto.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule, RedSocialModule, UsuarioModule, FotoModule],
   controllers: [AppController],
   providers: [AppService],
 })
