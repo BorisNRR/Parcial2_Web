e33213a9ea05a0d277cd0b5e61fa581774be7318
Boris Reyes <b.reyesr@uniandes.edu.co>
2023-12-02 12:57:48 -0500

    Correción en el móduslo de la entidad Red Social, importaba servicio erroneo.

--- src/red-social/red-social.module.ts
+++ src/red-social/red-social.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsuarioService } from './red-social.service';
+import { RedSocialService } from './red-social.service';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { RedSocialEntity } from './red-social.entity';
 
 
 @Module({
-  providers: [UsuarioService],
+  providers: [RedSocialService],
   imports:[TypeOrmModule.forFeature([RedSocialEntity])]
 })
 export class RedSocialModule {}
